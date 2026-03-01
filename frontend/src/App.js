1569ed9a21bf0c58d0cb39847e268692e2a0c4c6
Santanu-SP <santanupanchadhyai429@gmail.com>
2026-03-02 01:49:50 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PatientPage from './pages/PatientPage';
+import DriverPage from './pages/DriverPage';
+import HospitalPage from './pages/HospitalPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={{ padding: "20px" }}>
+        <h1>Emergency Response Intelligent System (ERIS)</h1>
+
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/patient">Patient</Link>
+          <Link to="/driver">Driver</Link>
+          <Link to="/hospital">Hospital</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/patient" element={<PatientPage />} />
+          <Route path="/driver" element={<DriverPage />} />
+          <Route path="/hospital" element={<HospitalPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
